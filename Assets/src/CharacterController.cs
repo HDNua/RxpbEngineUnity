d1cd66413c3239c69605b6fca008c7e58777eba1
DoYeong Han <rbfwmqwntm@naver.com>
2015-07-24 12:01:01 +0900

    character changed with X

--- Assets/src/CharacterController.cs
+++ Assets/src/CharacterController.cs
@@ -1,39 +1,121 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class CharacterController : MonoBehaviour
 {
-    Rigidbody2D _rigidbody2D;
+    #region 공용 GameObject 객체에 접근합니다.
+    Rigidbody2D _rigidbody;
+    Animator _animator;
 
-    // move left & right
-    public float maxSpeed = 10f;
-    bool facingRight = true;
+    #endregion
 
-    // animator controller
-    Animator anim;
 
-    // jump & not on the ground state
-    bool grounded = false;
-    public Transform groundCheck;
-    public float groundRadius = 0.2f;
-    public LayerMask whatIsGround;
-    public float jumpForce = 700f;
 
-    // shot
-    public Transform shotPosition;
-    public float shotSpeed = 20f;
-    public bool shotTriggered = false;
-
-    // override methods
-    void Start()
+    #region 상수를 정의합니다.
+    public enum GameKey
     {
-        _rigidbody2D = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
-        shotSpeed = maxSpeed * 2;
+        // 방향 키를 설정합니다.
+        Up,
+        Left,
+        Down,
+        Right,
+
+        // 일반 키를 설정합니다.
+        Jump,
+        Attack,
+        Dash,
+
+        // 확장 키를 설정합니다.
+
     }
 
+    #endregion 상수
+
+
+
+    #region Unity에서 접근하는 필드를 정의합니다.
+    // 이동 속성입니다.
+    public float walkSpeed = 8;
+    public float jumpSpeed = 16;
+    public float dashSpeed = 16;
+    public float dashForce = 800;
+
+    // 바닥 속성입니다.
+    public Transform groundCheck;
+    public float groundRadius = 0.01f;
+    public LayerMask whatIsGround;
+
+    #endregion Unity 필드
+
+
+
+    #region 외부에서 접근 가능한 필드를 정의합니다.
+    public Dictionary<GameKey, KeyCode> keySetting;
+
+    #endregion 공용 필드
+
+
+
+    #region 필드를 정의합니다.
+    bool facingRight;           // 우측을 보고 있다면 true입니다.
+    bool grounded;              // 땅에 닿아있다면 true입니다.
+    bool walking;               // 걷고 있다면 true입니다.
+    bool jumping;               // 점프 상태라면 true입니다.
+    bool dashing;               // 대쉬 상태라면 true입니다.
+
+    #endregion 필드 정의
+
+
+
+    #region 상속된 MonoBehaviour 메서드를 재정의합니다.
+    void Start()
+    {
+        // 공용 메서드를 초기화합니다.
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+
+        // 필드를 초기화합니다.
+        facingRight = true;
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        walking = false;
+        jumping = false;
+        dashing = false;
+
+        // 키 설정을 초기화합니다.
+        keySetting = new Dictionary<GameKey, KeyCode>();
+        keySetting[GameKey.Left] = KeyCode.LeftArrow;
+        keySetting[GameKey.Right] = KeyCode.RightArrow;
+        keySetting[GameKey.Jump] = KeyCode.C;
+        keySetting[GameKey.Attack] = KeyCode.X;
+        keySetting[GameKey.Dash] = KeyCode.Z;
+    }
     void Update()
     {
+        if (IsKeyDown(GameKey.Jump))
+        {
+            if (grounded == true)
+            {
+                jumping = true;
+                _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, jumpSpeed);
+            }
+        }
+        if (IsKeyDown(GameKey.Dash))
+        {
+            dashing = true;
+            _animator.SetBool("Dashing", true);
+            _animator.SetBool("DashKeyPressed", true);
+
+            float vx = facingRight ? dashSpeed : -dashSpeed;
+            _rigidbody.velocity = new Vector2(vx, _rigidbody.velocity.y);
+        }
+        if (IsKeyDown(GameKey.Left) || IsKeyDown(GameKey.Right))
+        {
+            walking = true;
+            _animator.SetBool("Walking", true);
+        }
+
+
+        /*
         if (Input.GetKeyDown(KeyCode.C))
         {
             anim.SetBool("Ground", false);
@@ -55,10 +137,73 @@ public class CharacterController : MonoBehaviour
         {
             print("TEST");
         }
+        */
     }
-
     void FixedUpdate()
     {
+        // 캐릭터 상태에 대한 부울 값을 나열합니다.
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        _animator.SetBool("Ground", grounded);
+
+        // 키 반응에 대한 부울 값을 나열합니다.
+        bool isLeftPressed = IsKeyPressed(GameKey.Left);
+        bool isRightPressed = IsKeyPressed(GameKey.Right);
+        bool isJumpPressed = IsKeyPressed(GameKey.Jump);
+        bool isDashPressed = IsKeyPressed(GameKey.Dash);
+
+        // 상태에 대한 반응을 나열합니다.
+        if (dashing) // 대시 중
+        {
+
+
+            
+
+            if (isDashPressed == false)
+            {
+                _animator.SetBool("DashKeyPressed", false);
+                _animator.SetBool("Dashing", false);
+                _rigidbody.velocity = new Vector2(_rigidbody.velocity.x / 4, _rigidbody.velocity.y);
+                dashing = false;
+            }
+        }
+        if (jumping) // 점프 중
+        {
+
+
+            if (isJumpPressed == false)
+            {
+                _animator.SetBool("JumpKeyPressed", false);
+            }
+        }
+
+
+        // 키에 대한 반응을 나열합니다.
+        if (walking)
+        {
+            if (dashing == false)
+            {
+                if (isLeftPressed)
+                {
+                    if (facingRight == true)
+                        Flip();
+                    _rigidbody.velocity = new Vector2(-walkSpeed, _rigidbody.velocity.y);
+                }
+                else if (isRightPressed)
+                {
+                    if (facingRight == false)
+                        Flip();
+                    _rigidbody.velocity = new Vector2(walkSpeed, _rigidbody.velocity.y);
+                }
+                else
+                {
+                    _rigidbody.velocity = new Vector2(0, _rigidbody.velocity.y);
+                    walking = false;
+                    _animator.SetBool("Walking", false);
+                }
+            }
+        }
+
+        /*
         // get state
         grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
         anim.SetBool("Ground", grounded);
@@ -91,9 +236,12 @@ public class CharacterController : MonoBehaviour
             anim.SetFloat("Speed", 0.0f);
             _rigidbody2D.velocity = new Vector2(0, _rigidbody2D.velocity.y);
         }
+        */
     }
 
-    // methods
+    #endregion MonoBehaviour 메서드 재정의
+
+    #region 사용자 정의 메서드를 정의합니다.
     void Flip()
     {
         facingRight = !facingRight;
@@ -101,4 +249,14 @@ public class CharacterController : MonoBehaviour
         theScale.x *= -1;
         transform.localScale = theScale;
     }
+    bool IsKeyDown(GameKey gameKey)
+    {
+        return Input.GetKeyDown(keySetting[gameKey]);
+    }
+    bool IsKeyPressed(GameKey gameKey)
+    {
+        return Input.GetKey(keySetting[gameKey]);
+    }
+
+    #endregion 사용자 정의 메서드
 }
