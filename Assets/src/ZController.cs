e29999e512b8eeb5e4d49457d17770ad4a8b43e4
DoYeong Han <rbfwmqwntm@naver.com>
2015-07-30 20:43:29 +0900

    Project become more stable
     프로젝트가 대단히 안정화되었습니다.
    - 주석이 강화되었습니다.
    - 플레이어의 행동과 상태가 분리되어 보다 효과적으로 작업할 수 있게 되었습니다.
    - 지형이 tux.png에서 실제 게임에 사용되는 스프라이트로 변경되었습니다.
    - 기타 많은 버그가 해결되었습니다.

--- Assets/src/ZController.cs
+++ Assets/src/ZController.cs
@@ -37,15 +37,42 @@ public class ZController : MonoBehaviour
     public LayerMask whatIsWall;
     public float wallCheckRadius = 0.1f;
 
+
+
+    public Transform groundCheckLeft;
+    public Transform groundCheckRight;
+
     #endregion Unity 공용 필드
 
 
 
-    #region 주인공의 상태 필드를 정의합니다.
+    #region 주인공의 상태 필드를 정의합니다. (deprecated)
     bool facingRight = true;
 
     bool jumping = false;
     bool falling = false;
+
+    #endregion
+
+
+
+    #region 주인공의 상태 필드를 정의합니다.
+    bool _moving = false;
+    bool _moveBlocked = false;
+
+    bool _jumping = false;
+    bool _jumpBlocked = false;
+    bool _falling = false;
+
+    bool _dashing = false;
+    bool _dashBlocked = false;
+
+    bool _attacking = false;
+    bool _attackBlocked = false;
+    bool _attackRequested = false;
+
+    bool _sliding = false;
+    bool _riding = false;
 
     #endregion
 
@@ -64,12 +91,17 @@ public class ZController : MonoBehaviour
             audioSources[i].clip = audioClips[i];
         }
 
-        RequestWalkBlock();
-//        _animator.SetBool("WalkBlocked", true);
-	}
-	void Update()
+        /*
+        var collider = GetComponent<BoxCollider2D>();
+        groundCheckLeft.position = new Vector2(collider.bounds.min.x, collider.bounds.min.y);
+        groundCheckRight.position = new Vector2(collider.bounds.max.x, collider.bounds.max.y);
+
+        print("TEST");
+        */
+    }
+    void _Update()
     {
-	    if (Input.GetKeyDown(KeyCode.X))
+        if (Input.GetKeyDown(KeyCode.X))
         {
             if (_animator.GetBool("ShotBlocked") == false)
             {
@@ -83,17 +115,9 @@ public class ZController : MonoBehaviour
                 RequestJump();
             }
         }
-	}
-    void FixedUpdate()
+    }
+    void _FixedUpdate()
     {
-        /*
-        BoxCollider2D collider = GetComponent<BoxCollider2D>();
-
-        bool grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
-        // Physics2D.OverlapArea(groundCheck2.bounds.min, groundCheck2.bounds.max, whatIsGround);
-        _animator.SetBool("Grounded", grounded);
-        bool grounded = _animator.GetBool("Grounded");
-        */
         bool grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
         _animator.SetBool("Grounded", grounded);
 
@@ -184,20 +208,437 @@ public class ZController : MonoBehaviour
         }
     }
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (_dashing)
+            {
+                DashJump();
+            }
+            else if (_jumping)
+            {
+
+            }
+            else
+            {
+                Jump();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            if (_jumping)
+            {
+                JumpAttack();
+            }
+            else if (_falling)
+            {
+                JumpAttack();
+            }
+            else if (_attackBlocked)
+            {
+
+            }
+            else
+            {
+                Attack();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (_jumping)
+            {
+                AirDash();
+            }
+            else if (_dashing)
+            {
+
+            }
+            else
+            {
+                Dash();
+            }
+        }
+
+    }
+    void FixedUpdate()
+    {
+        bool upPressed = Input.GetKey(KeyCode.UpArrow);
+        bool leftPressed = Input.GetKey(KeyCode.LeftArrow);
+        bool rightPressed = Input.GetKey(KeyCode.RightArrow);
+        bool jumpPressed = Input.GetKey(KeyCode.C);
+        bool dashPressed = Input.GetKey(KeyCode.Z);
+        bool attackPressed = Input.GetKey(KeyCode.X);
+
+        bool landed = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+        /*
+        bool leftLanded = Physics2D.OverlapCircle(groundCheckLeft.position, groundCheckRadius, whatIsGround);
+        bool rightLanded = Physics2D.OverlapCircle(groundCheckRight.position, groundCheckRadius, whatIsGround);
+        bool landed = leftLanded || rightLanded;
+        print(landed);
+        */
+
+        // 점프 중이라면
+        if (_jumping)
+        {
+            if (jumpPressed == false || _rigidbody.velocity.y <= 0)
+            {
+                StopJumping();
+                Fall();
+            }
+            else
+            {
+                _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y - 0.8f);
+            }
+        }
+        // 떨어지는 중이라면
+        else if (_falling)
+        {
+            if (landed)
+            {
+                StopFalling();
+            }
+            else
+            {
+                float vy = _rigidbody.velocity.y - 0.8f;
+                _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, vy > -16 ? vy : -16);
+            }
+        }
+        // 공격 중이라면
+        else if (_attacking)
+        {
+            if (_attackBlocked == false)
+            {
+                // 공격 방향을 전환하는 조건을 나열합니다.
+                if (leftPressed && facingRight)
+                {
+                    Flip();
+                }
+                else if (rightPressed && facingRight == false)
+                {
+                    Flip();
+                }
+            }
+        }
+        // 대쉬 중이라면
+        else if (_dashing)
+        {
+            // 대쉬를 취소하는 조건을 나열합니다.
+            if (leftPressed && facingRight)
+            {
+                StopDashing();
+                MoveLeft();
+            }
+            else if (rightPressed && facingRight == false)
+            {
+                StopDashing();
+                MoveRight();
+            }
+        }
+        // 사다리를 타는 중이라면
+        else if (_riding)
+        {
+
+        }
+        // 벽을 타고 있다면
+        else if (_sliding)
+        {
+
+        }
+        // 그 외의 경우
+        else
+        {
+            // 공중에 있다면 떨어지게 합니다.
+            if (landed == false)
+            {
+                Fall();
+            }
+        }
+
+        // 이동이 막혀있다면
+        if (_moveBlocked)
+        {
+        }
+        // 이동 가능한 상태라면
+        else
+        {
+            if (leftPressed)
+            {
+                if (_dashing)
+                {
+                    if (facingRight)
+                    {
+                        MoveLeft();
+                    }
+                }
+                else
+                {
+                    MoveLeft();
+                }
+            }
+            else if (rightPressed)
+            {
+                if (_dashing)
+                {
+                    if (facingRight == false)
+                    {
+                        MoveRight();
+                    }
+                }
+                else
+                {
+                    MoveRight();
+                }
+            }
+            else
+            {
+                StopMoving();
+            }
+        }
+    }
+
     #endregion MonoBehaviour 기본 메서드 재정의
 
 
 
-    #region 요청을 수행하기 위한 보조 메서드를 정의합니다.
-    void Flip()
+    #region 행동 메서드를 정의합니다.
+    void Idle()
     {
-        facingRight = !facingRight;
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
+        _jumping = false;
+        _falling = false;
+        _animator.SetBool("Moving", false);
     }
 
-    #endregion 보조 메서드 정의
+    ///////////////////////////////////////////////////////////////////
+    // 이동
+    /// <summary>
+    /// 플레이어를 왼쪽으로 이동합니다.
+    /// </summary>
+    void MoveLeft()
+    {
+        if (facingRight)
+            Flip();
+        _rigidbody.velocity = new Vector2(-walkSpeed, _rigidbody.velocity.y);
+        _animator.SetBool("Moving", true);
+    }
+    /// <summary>
+    /// 플레이어를 오른쪽으로 이동합니다.
+    /// </summary>
+    void MoveRight()
+    {
+        if (facingRight == false)
+            Flip();
+        _rigidbody.velocity = new Vector2(walkSpeed, _rigidbody.velocity.y);
+        _animator.SetBool("Moving", true);
+    }
+    /// <summary>
+    /// 플레이어의 이동을 중지합니다.
+    /// </summary>
+    void StopMoving()
+    {
+        _rigidbody.velocity = new Vector2(0, _rigidbody.velocity.y);
+        _animator.SetBool("Moving", false);
+    }
+    /// <summary>
+    /// 플레이어의 이동 요청을 막습니다.
+    /// </summary>
+    void BlockMoving()
+    {
+        _moveBlocked = true;
+    }
+    /// <summary>
+    /// 플레이어가 이동을 요청할 수 있도록 합니다.
+    /// </summary>
+    void UnblockMoving()
+    {
+        _moveBlocked = false;
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    // 점프 및 낙하
+    /// <summary>
+    /// 플레이어를 점프하게 합니다.
+    /// </summary>
+    void Jump()
+    {
+        _jumping = true;
+        _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, 16);
+        _animator.SetBool("Jumping", true);
+    }
+    /// <summary>
+    /// 플레이어의 점프를 중지합니다.
+    /// </summary>
+    void StopJumping()
+    {
+        _animator.SetBool("Jumping", false);
+    }
+    /// <summary>
+    /// 플레이어의 점프 요청을 막습니다.
+    /// </summary>
+    void BlockJumping()
+    {
+        _animator.SetBool("JumpBlocked", _jumpBlocked = true);
+    }
+    /// <summary>
+    /// 플레이어가 점프할 수 있도록 합니다.
+    /// </summary>
+    void UnblockJumping()
+    {
+        _animator.SetBool("JumpBlocked", _jumpBlocked = false);
+    }
+    /// <summary>
+    /// 플레이어를 낙하시킵니다.
+    /// </summary>
+    void Fall()
+    {
+        _jumping = false;
+        _falling = true;
+        _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, 0);
+        _animator.SetBool("Falling", true);
+    }
+    /// <summary>
+    /// 플레이어의 낙하를 중지합니다.
+    /// </summary>
+    void StopFalling()
+    {
+        _animator.SetBool("Falling", false);
+        _falling = false;
+
+        if (_attacking)
+        {
+            var curState = _animator.GetCurrentAnimatorStateInfo(0);
+            if (curState.IsName("Z_jumpShot_1inAir"))
+            {
+                var nTime = curState.normalizedTime;
+                var fTime = nTime - Mathf.Floor(nTime);
+                _animator.Play("Z_jumpShot_2ground", 0, fTime);
+
+                StopMoving();
+                BlockMoving();
+                audioSources[5].Play();
+            }
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    // 공격
+    /// <summary>
+    /// 플레이어가 공격하게 합니다.
+    /// </summary>
+    void Attack()
+    {
+        StopMoving();
+        BlockMoving();
+        _animator.SetBool("Attacking", _attacking = true);
+        _animator.SetBool("AttackRequested", _attackRequested = true);
+    }
+    /// <summary>
+    /// 플레이어의 공격을 중지합니다.
+    /// </summary>
+    void StopAttacking()
+    {
+        _animator.SetBool("Attacking", _attacking = false);
+        _animator.SetBool("AttackRequested", _attackRequested = false);
+        UnblockMoving();
+    }
+    /// <summary>
+    /// 플레이어의 공격 요청을 막습니다.
+    /// </summary>
+    void BlockAttacking()
+    {
+        _attackBlocked = true;
+    }
+    /// <summary>
+    /// 플레이어가 공격할 수 있도록 합니다.
+    /// </summary>
+    void UnblockAttacking()
+    {
+        _attackBlocked = false;
+    }
+    /// <summary>
+    /// 플레이어가 점프 공격하게 합니다.
+    /// </summary>
+    void JumpAttack()
+    {
+        _animator.SetBool("Attacking", _attacking = true);
+        _animator.SetBool("AttackRequested", _attackRequested = true);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    // 대쉬
+    /// <summary>
+    /// 플레이어가 대쉬하게 합니다.
+    /// </summary>
+    void Dash()
+    {
+        BlockMoving();
+        BlockDashing();
+    }
+    /// <summary>
+    /// 플레이어의 대쉬를 중지합니다.
+    /// </summary>
+    void StopDashing()
+    {
+        UnblockMoving();
+        UnblockDashing();
+    }
+    /// <summary>
+    /// 플레이어의 대쉬 요청을 막습니다.
+    /// </summary>
+    void BlockDashing()
+    {
+        _dashBlocked = true;
+    }
+    /// <summary>
+    /// 플레이어가 대쉬할 수 있도록 합니다.
+    /// </summary>
+    void UnblockDashing()
+    {
+        _dashBlocked = false;
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    // 기타
+    /// <summary>
+    /// 플레이어가 벽을 타도록 합니다.
+    /// </summary>
+    void Slide()
+    {
+        _animator.SetBool("Sliding", _sliding = true);
+    }
+    /// <summary>
+    /// 플레이어의 벽 타기를 중지합니다.
+    /// </summary>
+    void StopSliding()
+    {
+        _animator.SetBool("Sliding", _sliding = false);
+    }
+    /// <summary>
+    /// 플레이어가 사다리를 타도록 합니다.
+    /// </summary>
+    void RideLadder()
+    {
+
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    // 조합
+    /// <summary>
+    /// 플레이어가 에어 대쉬하게 합니다.
+    /// </summary>
+    void AirDash()
+    {
+
+    }
+    /// <summary>
+    /// 플레이어가 대쉬 점프하게 합니다.
+    /// </summary>
+    void DashJump()
+    {
+
+    }
+
+    #endregion
 
 
 
@@ -338,66 +779,144 @@ public class ZController : MonoBehaviour
     {
         RequestIdle();
     }
+
+    // 지상 공격 시에 발생하는 이벤트에 대한 핸들러입니다.
+    /// <summary>
+    /// 첫 번째 일반 지상 공격 시에 발생합니다.
+    /// </summary>
     public void Attack_saber1()
     {
-        _animator.SetBool("ShotRequested", false);
-        _animator.SetBool("JumpBlocked", true);
+        // 받은 요청은 삭제합니다.
+        _animator.SetBool("AttackRequested", _attackRequested = false);
+
+        // 공격 시 불가능한 행동을 막습니다.
+        BlockAttacking(); // 공격
+        BlockJumping(); // 점프
+        BlockDashing(); // 대쉬
+
+        // 효과음을 재생합니다.
         audioSources[0].Play();
         audioSources[3].Play();
     }
+    /// <summary>
+    /// 첫 번째 일반 지상 공격이 종료할 때 발생합니다.
+    /// </summary>
     public void Attack_saber1_end()
     {
-        _animator.SetBool("ShotBlocked", false);
-        _animator.SetBool("JumpBlocked", false);
+        // 막은 행동을 가능하게 합니다.
+        UnblockAttacking();
+        UnblockJumping();
+        UnblockDashing();
     }
+    /// <summary>
+    /// 두 번째 일반 지상 공격 시에 발생합니다.
+    /// </summary>
     public void Attack_saber2()
     {
-        _animator.SetBool("ShotRequested", false);
-        _animator.SetBool("JumpBlocked", true);
+        // 받은 요청은 삭제합니다.
+        _animator.SetBool("AttackRequested", _attackRequested = false);
+
+        // 공격 시 불가능한 행동을 막습니다.
+        BlockAttacking(); // 공격
+        BlockJumping(); // 점프
+        BlockDashing(); // 대쉬
+
+        // 효과음을 재생합니다.
         audioSources[1].Play();
         audioSources[3].Play();
     }
+    /// <summary>
+    /// 두 번째 일반 지상 공격이 종료할 때 발생합니다.
+    /// </summary>
     public void Attack_saber2_end()
     {
-        _animator.SetBool("ShotBlocked", false);
-        _animator.SetBool("JumpBlocked", false);
+        // 막은 행동을 가능하게 합니다.
+        UnblockAttacking();
+        UnblockJumping();
+        UnblockDashing();
     }
+    /// <summary>
+    /// 세 번째 일반 지상 공격 시에 발생합니다.
+    /// </summary>
     public void Attack_saber3()
     {
-        _animator.SetBool("ShotRequested", false);
-        _animator.SetBool("JumpBlocked", true);
+        // 받은 요청은 삭제합니다.
+        _animator.SetBool("AttackRequested", _attackRequested = false);
+
+        // 공격 시 불가능한 행동을 막습니다.
+        BlockAttacking(); // 공격
+        BlockJumping(); // 점프
+        BlockDashing(); // 대쉬
+
+        // 효과음을 재생합니다.
         audioSources[2].Play();
         audioSources[3].Play();
     }
+    /// <summary>
+    /// 지상 공격이 종료할 때 발생합니다.
+    /// </summary>
     public void AttackEndFromRun_beg()
     {
-        _animator.SetBool("ShotBlocked", false);
+        UnblockAttacking();
     }
+    /// <summary>
+    /// 지상 공격 모션이 완전히 종료되어 대기 상태로 바뀔 때 발생합니다.
+    /// </summary>
     public void AttackEndFromRun_end()
     {
-        _animator.SetBool("JumpBlocked", false);
-        _animator.SetBool("WalkBlocked", false);
+        StopAttacking();
     }
+
+    ///////////////////////////////////////////////////////////////////
+    // 점프 공격
+    /// <summary>
+    /// 점프 공격 시에 발생합니다.
+    /// </summary>
     public void JumpShot_beg()
     {
-        _animator.SetBool("ShotRequested", false);
-        _animator.SetBool("ShotBlocked", true);
+        BlockAttacking();
         audioSources[3].Play();
     }
+    /// <summary>
+    /// 점프 공격이 종료할 때 발생합니다.
+    /// </summary>
     public void JumpShot_end()
     {
-        _animator.SetBool("ShotBlocked", false);
+        UnblockAttacking();
+        StopAttacking();
     }
+
+    ///////////////////////////////////////////////////////////////////
+    // 벽 타기
+    /// <summary>
+    /// 벽 타기 시에 발생합니다.
+    /// </summary>
     public void SlideWall_beg()
     {
         _animator.SetBool("PushRequested", false);
         audioSources[7].Play();
 
     }
+    /// <summary>
+    /// 벽 타기가 종료할 때 발생합니다.
+    /// </summary>
     public void SlideWall_end()
     {
 
     }
 
     #endregion 프레임 이벤트 핸들러
+
+
+
+    #region 요청을 수행하기 위한 보조 메서드를 정의합니다.
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    #endregion 보조 메서드 정의
 }
