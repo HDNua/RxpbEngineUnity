f6d0e49c4d0801b0faf42db9107ae2927a54abdf
DoYeong Han <rbfwmqwntm@naver.com>
2015-08-06 22:47:33 +0900

    Big Improvement 15-08-06
    - 리소스 및 소스가 폴더 별로 분리되어 보다 명확하게 자원을 찾을 수 있게 되었습니다.
    > 더 이상 사용되지 않지만 이전에 사용하던 자원 및 그러한 자원에 대한
      폴더를 보관하는 _Trashbin 폴더가 추가되었습니다.
    > 장면을 찾으려면 Scene 폴더로 이동합니다.
    > 자원을 찾으려면 Resources 폴더로 이동합니다.
    > 소스를 찾으려면 src 폴더로 이동합니다.
    
    - 캐릭터 선택 장면 PlayerSelectScene이 추가되었습니다.
    > 캐릭터에 맞게 cutscene을 불러올 수 있습니다.
    
    - 장면 전환 모듈 LoadingSceneManager의 정적 메서드 LoadLevel의 인자가 변경되었습니다.
    > void LoadLevel(int levelIndex); -> void LoadLevel(string levelName);
    
    - 멧토 캐릭터가 이동하고, 타격 받을 수 있도록 개선되었습니다.
    > 체력 개념을 추가하여, 체력이 0 이하가 되면 소멸하도록 구현되었습니다.
    >> 캐릭터가 죽으면 explosion 효과가 나타납니다.
    > 벽 검사를 추가하여, 벽을 통과하여 이동하지 않도록 개선하였습니다.
    > 체력 변수를 public으로 지정하여 Unity에서 수정하는 것이 가능하도록 구현되었습니다.
    > AttackRange 객체와 겹칠 때 Hurt 메시지를 보내 대미지를 입도록 하였습니다.
    
    - 제로의 공격이 실제로 동작하도록 개선하였습니다.
    > 피격 판정을 위해 attackRange라는 객체가 추가되었습니다.
    > 피격당한 상대에 대해 EffectSliceHor 또는 EffectSliceVer 애니메이션이 발생합니다.
    ! 상대 캐릭터의 피격 타이밍이 이상합니다.
    ! 공중 공격이 구현되지 않았습니다.
    ! 대쉬 시에 공격 버튼이 눌려, 대쉬 키와 공격 버튼이 같이 들어온 경우
      대쉬 모션은 재생되면서 대쉬는 되지 않는 오류가 발생합니다.
    !> 대쉬 시에 공격 요청이 확인되면 대쉬를 중지하고 공격해야 합니다.
    !> 이후 발생하는 오류는 이 문제가 해결된 후 확인해야 합니다.
    
    
    Former-commit-id: a22c2344891533e18d2815cc515566c4c7ca68d9

---
+++ Assets/src/CutsceneManager.cs
@@ -0,0 +1,244 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CutsceneManager : MonoBehaviour
+{
+    string NEWLINE { get { return System.Environment.NewLine; } }
+    const char SEPERATOR = '|';
+
+    public GameObject textObject;
+    public AudioClip[] audioClips;
+    public TextAsset textAsset;
+
+    AudioSource[] _audioSources;
+    GUIText _guiText;
+
+    int subIndex;
+    int actionIndex;
+    List<string> subList;
+    List<string[]> actionList;
+
+    bool inputBlocked = false;
+    bool scriptPlaying = false;
+    bool EndScriptPlayingRequested = false;
+
+    void Start()
+    {
+        #region 필드 초기화를 진행합니다.
+        subList = new List<string>();
+        actionList = new List<string[]>();
+        _guiText = textObject.GetComponent<GUIText>();
+        subIndex = 0;
+        actionIndex = 0;
+
+        _audioSources = new AudioSource[audioClips.Length];
+        for (int i = 0, len = audioClips.Length; i < len; ++i)
+        {
+            _audioSources[i] = gameObject.AddComponent<AudioSource>();
+            _audioSources[i].clip = audioClips[i];
+        }
+
+        #endregion
+
+        #region 텍스트 파일로부터 대사를 획득하고 분석합니다.
+        //        string path = System.Environment.CurrentDirectory + "\\Assets\\Cutscene\\CS01_Intro.txt";
+        //        string text = System.IO.File.ReadAllText(path, System.Text.Encoding.UTF8);
+
+        string text = textAsset.text;
+        string[] subscriptArray = text.Replace(NEWLINE, "|").Split('|');
+
+        string newSub = null;
+        for (int i = 0, len = subscriptArray.Length; i < len; ++i)
+        {
+            string subscriptElem = subscriptArray[i];
+            if (subscriptElem == null)
+            {
+                throw new System.Exception("INVALID SUBSCRIPT LINE");
+            }
+            else if (subscriptElem == string.Empty)
+            {
+                if (newSub != null)
+                {
+                    newSub += (SEPERATOR);
+                }
+                continue;
+            }
+
+            // 대사라면
+            if (subscriptElem == "[")
+            {
+                newSub = "";
+            }
+            else if (subscriptElem == "]")
+            {
+//                print(newSub);
+                subList.Add(newSub);
+                newSub = null;
+            }
+            // 명령이라면
+            else if (subscriptElem[0] == '(')
+            {
+                actionList.Add(subscriptElem.Substring(1, subscriptElem.Length - 2).Split(' '));
+                subList.Add("$");
+                continue;
+            }
+            // 그 외의 경우
+            else
+            {
+                if (newSub == null)
+                    throw new System.Exception("INVALID SUBSCRIPT SEPERATOR");
+                newSub += (subscriptElem + SEPERATOR);
+            }
+        }
+
+        #endregion
+
+        // 행동을 시작합니다.
+        DoNextScript();
+    }
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            if (inputBlocked)
+            {
+
+            }
+            else if (scriptPlaying)
+            {
+                EndScriptPlayingRequested = true;
+            }
+            else
+            {
+                DoNextScript();
+            }
+        }
+    }
+
+    void DoNextScript()
+    {
+        if (subIndex >= subList.Count)
+        {
+            return;
+        }
+
+        char check = subList[subIndex][0];
+        if (check == '$')
+        {
+            ++subIndex;
+            scriptPlaying = true;
+            inputBlocked = true;
+            Act();
+        }
+        else
+        {
+            scriptPlaying = true;
+            inputBlocked = true;
+            ShowNextSubscript();
+        }
+    }
+
+    void ShowNextSubscript()
+    {
+        _guiText.text = "";
+        StartCoroutine(ShowSubscriptChar());
+    }
+    void Act()
+    {
+        inputBlocked = true;
+        if (actionIndex < actionList.Count)
+        {
+            _guiText.text = "";
+            StartCoroutine(ActWithCoroutine());
+        }
+    }
+
+    IEnumerator ActWithCoroutine()
+    {
+        string[] action = actionList[actionIndex++];
+        string command = action[0];
+        switch (command)
+        {
+            case "WaitForSeconds":
+                float sec = float.Parse(action[1]);
+                yield return new WaitForSeconds(sec);
+                break;
+            case "Warning":
+                _audioSources[1].Play();
+                yield return new WaitForSeconds(1);
+                break;
+            case "Load":
+                string levelName = action[1]; // int level = int.Parse(action[1]);
+                LoadingSceneManager.LoadLevel(levelName);
+                break;
+        }
+        DoNextScript();
+    }
+
+    IEnumerator ShowSubscriptChar()
+    {
+        string subscript = subList[subIndex++];
+        string name = null;
+
+        for (int i = 0, len = subscript.Length; i < len; ++i)
+        {
+            char subChar = subscript[i];
+
+            // 콜론으로 시작하는 문장은 즉시 출력합니다.
+            // 대화 시 캐릭터의 이름에 사용합니다.
+            if (subChar == ':')
+            {
+                ++i;
+                name = "";
+                while (i < len)
+                {
+                    if (subscript[i] == SEPERATOR)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        name += subscript[i];
+                    }
+                    ++i;
+                }
+                _guiText.text += name + NEWLINE;
+            }
+            // 분리자인 경우 개행합니다.
+            else if (subChar == SEPERATOR)
+            {
+                _guiText.text += NEWLINE;
+            }
+            // 스크립트 재생이 사용자에 의해 종료된 경우
+            else if (EndScriptPlayingRequested)
+            {
+                name = "";
+                while (i < len)
+                {
+                    if (subscript[i] == SEPERATOR)
+                    {
+                        name += NEWLINE;
+                    }
+                    else
+                    {
+                        name += subscript[i];
+                    }
+                    ++i;
+                }
+                _guiText.text += name;
+                EndScriptPlayingRequested = false;
+                scriptPlaying = false;
+            }
+            // 글자 단위로 소리를 내며 출력합니다.
+            else
+            {
+                _guiText.text += subChar;
+                _audioSources[0].Play();
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+        scriptPlaying = false;
+        inputBlocked = false;
+    }
+}
---
+++ Assets/src/LoadingSceneManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadingSceneManager : MonoBehaviour
+{
+    public ScreenFader fader;
+    bool fadeRequested = false;
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        if (loadRequested)
+        {
+//            if (loadingLevel < 0)
+//                return;
+            StartCoroutine(LoadMain());
+            loadRequested = false;
+        }
+    }
+
+    static bool loadRequested = false;
+    static int loadingLevel = -1;
+
+    static string loadingLevelName = null;
+
+    IEnumerator LoadMain()
+    {
+        AsyncOperation async = Application.LoadLevelAsync(loadingLevelName);
+        while (async.isDone == false)
+        {
+            if (async.progress >= 0.8f)
+            {
+                if (fadeRequested == false)
+                {
+                    if (fader != null)
+                    {
+                        fader.RequestSceneEnd();
+                    }
+                    fadeRequested = true;
+                }
+            }
+            yield return true;
+        }
+    }
+
+    /*
+    public static void LoadLevel(int level)
+    {
+        loadingLevel = level;
+        loadRequested = true;
+        Application.LoadLevel(1);
+    }
+    */
+    public static void LoadLevel(string levelName)
+    {
+        loadingLevelName = levelName;
+        loadRequested = true;
+        Application.LoadLevel("Loading");
+    }
+}
