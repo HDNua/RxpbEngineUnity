04212a616dd885cc4cf7fccf3a999ba514bb5a3f
DoYeong Han <rbfwmqwntm@naver.com>
2017-03-14 12:14:19 +0900

    ﻿RXPB v5.3.0
    ! CrouchShotLight와 같은 상태에 대해서도 RXPBHelper로 팔레트 수정 요망.
    
    ! 앉은 상태에서 대쉬하거나, 대쉬 종료 시에 앉은 상태 키가 눌려있는 경우
      앉은 상태에서 미끄러지는 모션이 필요해보인다.
    
    ! 보스 전투 구현해야 함.
    
    - BattonBone 적 캐릭터 구현.
    
    end.
    
    
    Former-commit-id: 1ee19f98f1f00a900ad24b6975a595a27e86f57d

---
+++ Assets/EnemyBattonBoneScript.cs
@@ -0,0 +1,318 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+
+
+/// <summary>
+/// 박쥐 적 캐릭터를 정의합니다.
+/// </summary>
+public class EnemyBattonBoneScript : EnemyScript
+{
+    #region 컨트롤러가 사용할 Unity 객체를 정의합니다.
+    /// <summary>
+    /// Rigidbody2D 요소를 가져옵니다.
+    /// </summary>
+    Rigidbody2D _rigidbody;
+    /// <summary>
+    /// BoxCollider2D 요소를 가져옵니다.
+    /// </summary>
+    Collider2D _collider2D;
+
+    #endregion
+
+
+
+
+
+    #region Unity에서 접근 가능한 공용 객체를 정의합니다.
+    /// <summary>
+    /// 자신의 밑에 지면이 존재하는지 검사하기 위해 사용합니다.
+    /// </summary>
+    public Transform _groundCheck;
+    /// <summary>
+    /// 자신이 진행하는 방향에 벽이 존재하는지 검사하기 위해 사용합니다.
+    /// </summary>
+    public Transform _pushCheck;
+    /// <summary>
+    /// 무엇이 벽인지를 결정합니다. 기본값은 "Wall, MapBlock"입니다.
+    /// </summary>
+    public LayerMask _whatIsWall;
+    /// <summary>
+    /// 무엇이 땅인지를 결정합니다. 기본값은 "Ground, TiledGeometry"입니다.
+    /// </summary>
+    public LayerMask _whatIsGround;
+
+    #endregion
+
+
+
+
+
+
+
+
+
+
+    #region 캐릭터의 상태 필드 및 프로퍼티를 정의합니다.
+    /// <summary>
+    /// 캐릭터가 움직이는 속도를 정의합니다.
+    /// </summary>
+    public float _movingSpeed = 1;
+    /// <summary>
+    /// 캐릭터가 도망치는 속도를 정의합니다.
+    /// </summary>
+    public float _runawaySpeed = 2;
+
+    #endregion
+
+
+
+
+
+
+
+
+
+
+    #region MonoBehaviour 기본 메서드를 재정의합니다.
+    /// <summary>
+    /// MonoBehaviour 개체를 초기화합니다.
+    /// </summary>
+    protected override void Start()
+    {
+        base.Start();
+
+
+        // 필드를 초기화합니다.
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _collider2D = GetComponent<Collider2D>();
+
+        // 자신과 가장 가까운 바닥으로 y 좌표를 옮깁니다.
+        /**
+        RaycastHit2D groundRay = Physics2D.Raycast
+            (_groundCheck.position, Vector2.down, 10f, _whatIsGround);
+        Vector2 newPos = transform.position;
+        newPos.y -= Mathf.Abs(_collider2D.bounds.min.y - groundRay.point.y);
+        transform.position = newPos;
+        */
+
+        MoveLeft();
+    }
+    /// <summary>
+    /// 프레임이 갱신될 때 MonoBehaviour 개체 정보를 업데이트합니다.
+    /// </summary>
+    protected override void Update()
+    {
+        base.Update();
+
+
+        if (false)
+        {
+            Vector2 relativePos = _StageManager.GetCurrentPlayerPosition() - transform.position;
+            float angle = Mathf.Atan2(relativePos.y, relativePos.x) * Mathf.Rad2Deg;
+            // transform.localRotation = Quaternion.Euler(0, 0, angle - 90);
+            transform.Translate(transform.up * _movingSpeed * Time.deltaTime, Space.World);
+        }
+        else
+        {
+            // 사용할 변수를 선언합니다.
+            Vector3 playerPos = _StageManager.GetCurrentPlayerPosition();
+
+            /*
+            Vector3 diff = playerPos - transform.position;
+            float angle = Vector2.Angle(Vector2.right, diff);
+            if (Vector3.Cross(Vector3.right, diff).z < 0)
+                angle = 360 - angle;
+            */
+
+            Vector2 relativePos = _StageManager.GetCurrentPlayerPosition() - transform.position;
+            float angle = Mathf.Atan2(relativePos.y, relativePos.x); // * Mathf.Rad2Deg;
+
+            float vx = _movingSpeed * Mathf.Cos(angle);
+            float vy = _movingSpeed * Mathf.Sin(angle);
+
+            Handy.Log("PlayerPosition={0}, Position={1}, Angle={2}, vx={3}, vy={4}", playerPos, transform.position, angle, vx, vy);
+            _rigidbody.velocity = new Vector2(vx, vy);
+
+
+            // 
+            if (relativePos.x < 0 && FacingRight)
+                Flip();
+            else if (relativePos.x > 0 && !FacingRight)
+                Flip();
+
+
+
+            /*
+            float boundLeft = SpawnZone.Left;
+            float boundRight = SpawnZone.Right;
+
+            // 영역을 넘어서면 방향을 전환하여 원래대로 복귀합니다.
+            if (posX < boundLeft)
+            {
+                MoveRight();
+            }
+            else if (boundRight < posX)
+            {
+                MoveLeft();
+            }
+            */
+        }
+    }
+
+
+    #endregion
+
+
+
+
+
+
+
+
+
+
+    #region Collider2D의 기본 메서드를 재정의합니다.
+    /// <summary>
+    /// 충돌체가 여전히 트리거 내부에 있습니다.
+    /// </summary>
+    /// <param name="other">자신이 아닌 충돌체 개체입니다.</param>
+    void OnTriggerStay2D(Collider2D other)
+    {
+        // 트리거가 발동한 상대 충돌체가 플레이어라면 대미지를 입힙니다.
+        if (other.CompareTag("Player"))
+        {
+            GameObject pObject = other.gameObject;
+            PlayerController player = pObject.GetComponent<PlayerController>();
+
+
+            // 플레이어가 무적 상태이거나 죽었다면
+            if (player.Invencible || player.IsDead)
+            {
+                // 아무 것도 하지 않습니다.
+
+            }
+            // 그 외의 경우
+            else
+            {
+                // 플레이어에게 대미지를 입힙니다.
+                player.Hurt(Damage);
+            }
+        }
+    }
+
+
+    #endregion
+
+
+
+
+
+
+
+
+
+
+    #region EnemyScript의 메서드를 오버라이드합니다.
+    /// <summary>
+    /// 캐릭터가 사망합니다.
+    /// </summary>
+    public override void Dead()
+    {
+        // 폭발 효과를 생성하고 효과음을 재생합니다.
+        SoundEffects[0].Play();
+        Instantiate(effects[0], transform.position, transform.rotation);
+
+        // 사망 시 아이템 드롭 루틴입니다.
+        int dropItem = UnityEngine.Random.Range(0, _items.Length);
+        if (_items[dropItem] != null)
+        {
+            CreateItem(_items[dropItem]);
+        }
+
+        // 캐릭터가 사망합니다.
+        base.Dead();
+
+        // 
+        Invoke("RequestDestroy", 3f);
+    }
+
+
+    #endregion
+
+
+
+
+
+
+
+
+
+
+    #region 보조 메서드를 정의합니다.
+    /// <summary>
+    /// 왼쪽으로 이동합니다.
+    /// </summary>
+    void MoveLeft()
+    {
+        if (FacingRight)
+            Flip();
+        _rigidbody.velocity = new Vector2(-_movingSpeed, 0);
+    }
+    /// <summary>
+    /// 오른쪽으로 이동합니다.
+    /// </summary>
+    void MoveRight()
+    {
+        if (FacingRight == false)
+            Flip();
+        _rigidbody.velocity = new Vector2(_movingSpeed, 0);
+    }
+    /// <summary>
+    /// 주변을 방황합니다.
+    /// </summary>
+    /// <returns>StartCoroutine 호출에 적합한 값을 반환합니다.</returns>
+    IEnumerator WalkAround()
+    {
+        while (_health != 0)
+        {
+            int random = UnityEngine.Random.Range(0, 2);
+            if (random == 1)
+            {
+                MoveLeft();
+            }
+            else
+            {
+                MoveRight();
+            }
+            yield return new WaitForSeconds(3);
+        }
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void RequestDestroy()
+    {
+        Destroy(gameObject);
+    }
+
+
+    #endregion
+
+
+
+
+
+
+
+
+
+
+    #region 구형 정의를 보관합니다.
+
+
+    #endregion
+}
